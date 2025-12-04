ea1b206717ffd6d725067f43542dc34d952f8547
AHMEDHEGAZY83 <sofamido83@gmail.com>
2025-12-04 10:26:54 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Navigate to pocketbase folder
 cd pocketbase
-
-# Make sure pocketbase is executable
 chmod +x pocketbase
-
-# Start PocketBase with admin UI
-./pocketbase serve --http="0.0.0.0:${PORT}" --publicDir="pb_public"
+# publicDir يشير لمجلد pb_public في مستوى أعلى
+./pocketbase serve --http="0.0.0.0:${PORT}" --publicDir="../pb_public"
